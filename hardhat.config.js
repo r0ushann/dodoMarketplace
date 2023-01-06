1a0ce1a77bfd3fb675a349677ae3854eae4f1645
ROUSHAN KUMAR <86906965+r0ushann@users.noreply.github.com>
2023-01-06 10:28:42 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomiclabs/hardhat-waffle");
 
 module.exports = {
+    defaultNetwork: "mumbai",
+    networks: {
+        hardhat: {},
+        mumbai: {
+            url: process.env.STAGING_ALCHEMY_KEY,
+            accounts: [process.env.PRIVATE_KEY]
+        }
+    },
   solidity: "0.8.4",
   
   paths: {
